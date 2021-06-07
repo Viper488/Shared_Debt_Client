b219598d0506fe3e58419d3e5b29e68f30ca64d7
Artur Hamernik <hamernik18@gmail.com>
2021-06-07 14:28:02 +0200

    links

--- app/src/main/java/com/example/sdclient/HttpSevice.java
+++ app/src/main/java/com/example/sdclient/HttpSevice.java
@@ -19,49 +19,49 @@ import retrofit2.http.Query;
 
 public interface HttpSevice {
 
-    @POST ("ud-server/login")
+    @POST ("sd-server/login")
     Call<Map<String,String>> login(@Body LoginDto loginDto);
 
-    @POST("ud-server/register")
+    @POST("sd-server/register")
     Call<Void> register(@Body RegisterDto registerDto);
 
-    @POST("ud-server/create_meeting")
+    @POST("sd-server/create_meeting")
     Call<String> createMeeting(@Query("name") String name , @Query("password") String password);
 
-    @POST("ud-server/join_meeting")
+    @POST("sd-server/join_meeting")
     Call<Void> joinMeeting(@Query("name") String name , @Query("password") String password);
 
-    @GET("ud-server/meeting_details_code")
+    @GET("sd-server/meeting_details_code")
     Call<MeetingDetailsDto> getMeetingDetails(@Query("code") String code);
 
-    @GET("ud-server/person_meetings")
+    @GET("sd-server/person_meetings")
     Call<MeetingListDto> getPersonsMeetingList(@Query("id_person") String idPerson);
 
-    @GET("ud-server/products")
+    @GET("sd-server/products")
     Call<ProductListDto> getMeetingsProducts(@Query("id_meeting") String id_meeting);
 
-    @POST("ud-server/delete_product")
+    @POST("sd-server/delete_product")
     Call<Void> deleteProduct(@Query("id_product") String id_product);
 
-    @POST("ud-server/add_person")
+    @POST("sd-server/add_person")
     Call<Void> addPerson(@Query("id_meeting") String id_meeting,@Query("nick") String nick);
 
-    @POST("ud-server/product")
+    @POST("sd-server/product")
     Call<Void> addProduct(@Query("name") String name,@Query("price") String price,@Query("id_person") String id_person,@Query("id_meeting") String id_meeting);
 
-    @GET("ud-server/payments_meeting/{id_meeting}")
+    @GET("sd-server/payments_meeting/{id_meeting}")
     Call<PaymentListDto> getMeetingsPayments(@Path("id_meeting") String id_meeting);
 
-    @POST("ud-server/payment")
+    @POST("sd-server/payment")
     Call<Void> insertPayment(@Body PaymentDto paymentDto);
 
-    @GET("ud-server/payments_sum_meeting/{id_meeting}")
+    @GET("sd-server/payments_sum_meeting/{id_meeting}")
     Call<Double> getMeetingsPayment(@Path("id_meeting") String id_meeting);
 
-    @GET("ud-server/payments_sum_person/{id_person}")
+    @GET("sd-server/payments_sum_person/{id_person}")
     Call<Double> getSumPersonPayments(@Path("id_person") String id_person);
 
-    @GET("ud-server/payments_person/{id_person}")
+    @GET("sd-server/payments_person/{id_person}")
     Call<PaymentListDto> getPersonPayments(@Path("id_person") String id_person);
 
 
